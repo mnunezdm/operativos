34b6a71e02b295d88ae22be6dd97af3fb5ba14e1
Miguel Núñez Díaz-Montes <m.nunezd@alumnos.upm.es>
2016-11-09 14:27:39 +0100

    Arreglado todo recorrer directorio, deberia pasar todas las pruebas

--- libfiltra_alfa.c
+++ libfiltra_alfa.c
@@ -5,8 +5,7 @@
 
 /* Este filtro deja pasar los caracteres NO alfabeticos. */
 /* Devuelve el numero de caracteres que han pasado el filtro. */
-int tratar(char* buff_in, char* buff_out, int tam)
-{
+int tratar(char* buff_in, char* buff_out, int tam) {
 	int o = 0; 
 
 	return o;
